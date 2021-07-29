bec990a650fa5340cc6007775dfbe55e683ed66a
PotapovaMarina <potapova_me@inbox.ru>
2021-07-29 23:49:29 +0300

    Домашнее задание №7: 1. Создано боковое навигационное меню для приложения. При выборе пунктов меню "Главная" и "О приложении" открываются соответствующие фрагменты. 2. Создано верхнее меню и Toolbar 3. Создано popup меню с пунктами : "Открыть заметку", "Добавить заметку", "Очистить все заметки"

--- app/src/main/java/com/example/mynote_lesson6/NoteListFragment.java
+++ app/src/main/java/com/example/mynote_lesson6/NoteListFragment.java
@@ -2,14 +2,17 @@ package com.example.mynote_lesson6;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.button.MaterialButton;
@@ -18,15 +21,19 @@ import java.util.ArrayList;
 
 public class NoteListFragment extends Fragment {
     private LinearLayout linearLayout;
+    private static final int FLAG_EDIT = 1;
+    private static final int FLAG_NEW = 0;
     private MaterialButton createNoteButton;
     public static final int TEXT_ALIGNMENT_TEXT_START = 2;
+    private View view;
+    private ScrollView scrollView;
     private ArrayList<NoteEntity> noteList = new ArrayList<>();
 
     public void addNoteToList(NoteEntity noteEntity) {
-        if (noteEntity != null && noteEntity.flag == 0) {
+        if (noteEntity != null && noteEntity.flag == FLAG_NEW) {
             noteList.add(noteEntity);
             addButtonWithNote(noteEntity);
-        } else if (noteEntity != null && noteEntity.flag == 1) {
+        } else if (noteEntity != null && noteEntity.flag == FLAG_EDIT) {
             linearLayout.removeAllViewsInLayout();
             for (NoteEntity note : noteList) {
                 addButtonWithNote(note);
@@ -37,8 +44,9 @@ public class NoteListFragment extends Fragment {
     public void addButtonWithNote(NoteEntity noteEntity) {
         Button button = new Button(getContext());
         button.setText(noteEntity.toString());
-        button.setOnClickListener(v -> ((Controller) getActivity()).openProfileScreen(noteEntity));
-
+        button.setOnClickListener(v -> {
+            initPopupMenu(view, noteEntity);
+        });
         LinearLayout.LayoutParams buttonparam = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -49,7 +57,32 @@ public class NoteListFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_note_list, null);
+        view = inflater.inflate(R.layout.fragment_note_list, null);
+        return view;
+    }
+
+    private void initPopupMenu(View view, NoteEntity noteEntity) {
+        PopupMenu popupMenu = new PopupMenu(getActivity(), linearLayout, Gravity.TOP);
+        getActivity().getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+        Menu menu = popupMenu.getMenu();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.menu_clear:
+                        linearLayout.removeAllViewsInLayout();
+                        return true;
+                    case R.id.menu_add:
+                        ((Controller) getActivity()).createNewNote();
+                        return true;
+                    case R.id.menu_open:
+                        ((Controller) getActivity()).openProfileScreen(noteEntity);
+                }
+                return true;
+            }
+        });
+        popupMenu.show();
     }
 
     @Override
@@ -63,15 +96,16 @@ public class NoteListFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         linearLayout = view.findViewById(R.id.linear);
+        scrollView = view.findViewById(R.id.scroll_view);
         createNoteButton = view.findViewById(R.id.create_button);
         createNoteButton.setOnClickListener(v -> {
             ((Controller) getActivity()).createNewNote();
         });
+        setHasOptionsMenu(true);
     }
 
     interface Controller {
         void openProfileScreen(NoteEntity note);
-
         void createNewNote();
     }
 }
